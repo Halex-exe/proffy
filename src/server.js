dbfacdd3c441c5ae620590667081938c15cc23cd
Alexandre Klafke <alexandre.m.klafke@gmail.com>
2020-08-09 18:46:55 -0300

    back-end refactoring and add data(hardcoding)

--- src/server.js
+++ src/server.js
@@ -1,15 +1,46 @@
+const proffys = [
+    {
+    name: "Alexandre Klafke", 
+    avatar: "https://avatars0.githubusercontent.com/u/33735642?s=460&u=506a0c9779f66084bf6e61aed77c173fb97b85b9&v=4", 
+    whatsapp: "55 51 999999999", 
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br><br>Donec metus justo, posuere eget nulla quis, consectetur tempus odio. Donec in risus non purus ullamcorper rutrum.", 
+    subject: "Math", 
+    cost: "20", 
+    weekday: [0], 
+    time_from: [700], 
+    time_to: [1200]
+},
+{
+    name: "Alexandre Klafke 2", 
+    avatar: "https://avatars0.githubusercontent.com/u/33735642?s=460&u=506a0c9779f66084bf6e61aed77c173fb97b85b9&v=4", 
+    whatsapp: "55 51 999999999", 
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br><br>Donec metus justo, posuere eget nulla quis, consectetur tempus odio. Donec in risus non purus ullamcorper rutrum.", 
+    subject: "Math", 
+    cost: "30", 
+    weekday: [1], 
+    time_from: [700], 
+    time_to: [1200]
+    }
+]
+
+function pageLanding(req, res){
+    return res.sendFile(__dirname + "/views/index.html")
+}
+
+function pageStudy (req, res){
+    return res.sendFile(__dirname + "/views/study.html")
+}
+
+function pageGiveClasses(req, res){
+    return res.sendFile(__dirname + "/views/give-classes.html")
+}
+
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
-
-.get("/", function(req, res){
-    return res.sendFile(__dirname + "/views/index.html")
-})
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-})
+server
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(5500)
